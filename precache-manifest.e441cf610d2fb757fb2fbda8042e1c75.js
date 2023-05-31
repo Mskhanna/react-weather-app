75a8bf096cacbc3ed4d98ec55841a39330e45690
Manu Khanna <2manukhanna@gmail.com>
2023-05-30 20:13:11 -0400

    Updates

---
+++ precache-manifest.e441cf610d2fb757fb2fbda8042e1c75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18580e84cf6ed3a6eebca6cc14667f10",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "1fe975f695b9014affa1",
+    "url": "/react-weather-app/static/css/main.d6c3555f.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/react-weather-app/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe975f695b9014affa1",
+    "url": "/react-weather-app/static/js/main.691728db.chunk.js"
+  },
+  {
+    "revision": "1ef2e9538f1d2cafc04e",
+    "url": "/react-weather-app/static/js/runtime-main.7bf44560.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/react-weather-app/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/react-weather-app/static/media/city.e1698332.jpg"
+  }
+]);
